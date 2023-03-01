ed5338ef93a8cacc72e5c88e12ccb1ae13adc115
reedsource <reedsource@189.cn>
2023-03-01 17:42:55 +0800

    更新 定时任务违规的字符添加

--- reed-common/src/main/java/top/reed/common/constant/Constants.java
+++ reed-common/src/main/java/top/reed/common/constant/Constants.java
@@ -110,5 +110,5 @@ public class Constants {
 	 * 定时任务违规的字符
 	 */
 	public static final String[] JOB_ERROR_STR = {"java.net.URL", "javax.naming.InitialContext", "org.yaml.snakeyaml",
-			"org.springframework", "org.apache", "top.reed.common.utils.file"};
+			"org.springframework", "org.apache", "com.reed.common.utils.file", "com.reed.common.config"};
 }
